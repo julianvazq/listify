44b45f7ac44e7925b6b518b975d5ed6944797c73
Julian Vazquez <julianvazquez101@gmail.com>
2020-08-13 00:20:42 -0400

    Remove unused endpoints

--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../db/pool');
-const { json } = require('express');
 
-router.get('/', (req, res) => {
-  res.send(JSON.stringify('Hello there.'));
-});
-
+/* Endpoint for lists' preview */
 router.get('/list/:id', async (req, res) => {
   const { id } = req.params;
   const newList = await pool.query(
@@ -14,31 +10,6 @@ router.get('/list/:id', async (req, res) => {
     [id]
   );
   res.send(newList.rows).status(200);
-});
-
-// SELECT ALL
-router.get('/query', async (req, res) => {
-  const list = await pool.query('SELECT * FROM public.lists');
-  console.log(list.rows[0]);
-});
-
-// GET BY ID
-router.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  const list = await pool.query('SELECT * FROM public.lists WHERE id = $1', [
-    id,
-  ]);
-  console.log(list.rows);
-});
-
-// ADD LIST
-router.post('/:id', async (req, res) => {
-  const { id } = req.params;
-  const newList = await pool.query(
-    'INSERT INTO public.lists (id) VALUES($1) RETURNING *',
-    [id]
-  );
-  console.log(newList.rows);
 });
 
 module.exports = router;
