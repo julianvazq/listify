d48207669ab72424ecb91cc49bb5ed2c84b36b03
Julian Vazquez <julianvazquez101@gmail.com>
2020-09-07 17:48:05 -0400

    Optimize images / Fix bug

--- sockets/updateUserNameHandler.js
+++ sockets/updateUserNameHandler.js
@@ -1,10 +1,21 @@
 const { GET_ITEMS } = require('../db/queries/item');
-const { io } = require('../server');
-const { UPDATE_USER_NAME } = require('../db/queries/user');
+const {
+  UPDATE_USER_NAME,
+  CHECK_USER,
+  CREATE_USER,
+} = require('../db/queries/user');
 
 module.exports = (event, socket, io) => {
   socket.on(event, async ({ listId, user, fromHome }, callback) => {
-    const updatedUser = await UPDATE_USER_NAME(user);
+    const userExists = await CHECK_USER(user.id);
+
+    let updatedUser;
+
+    if (userExists) {
+      updatedUser = await UPDATE_USER_NAME(user);
+    } else {
+      updatedUser = await CREATE_USER(user);
+    }
 
     if (!fromHome) {
       const items = await GET_ITEMS(listId);
